f3526493b950c5909f84c23a9239bc5221bed214
ggare-cmu <ggare@andrew.cmu.edu>
2020-05-07 22:40:48 -0400

    initial

---
+++ MultiCol_SLAM/build/CMakeFiles/MultiCol-SLAM.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/src/cConverter.cpp" "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/MultiCol-SLAM.dir/src/cConverter.o"
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/src/cLocalMapping.cpp" "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/MultiCol-SLAM.dir/src/cLocalMapping.o"
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/src/cLoopClosing.cpp" "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/MultiCol-SLAM.dir/src/cLoopClosing.o"
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/src/cMap.cpp" "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/MultiCol-SLAM.dir/src/cMap.o"
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/src/cMapPoint.cpp" "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/MultiCol-SLAM.dir/src/cMapPoint.o"
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/src/cMapPublisher.cpp" "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/MultiCol-SLAM.dir/src/cMapPublisher.o"
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/src/cMultiFrame.cpp" "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/MultiCol-SLAM.dir/src/cMultiFrame.o"
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/src/cMultiFramePublisher.cpp" "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/MultiCol-SLAM.dir/src/cMultiFramePublisher.o"
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/src/cMultiInitializer.cpp" "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/MultiCol-SLAM.dir/src/cMultiInitializer.o"
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/src/cMultiKeyFrame.cpp" "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/MultiCol-SLAM.dir/src/cMultiKeyFrame.o"
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/src/cMultiKeyFrameDatabase.cpp" "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/MultiCol-SLAM.dir/src/cMultiKeyFrameDatabase.o"
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/src/cORBmatcher.cpp" "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/MultiCol-SLAM.dir/src/cORBmatcher.o"
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/src/cOptimizer.cpp" "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/MultiCol-SLAM.dir/src/cOptimizer.o"
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/src/cOptimizerLoopStuff.cpp" "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/MultiCol-SLAM.dir/src/cOptimizerLoopStuff.o"
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/src/cSim3Solver.cpp" "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/MultiCol-SLAM.dir/src/cSim3Solver.o"
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/src/cSystem.cpp" "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/MultiCol-SLAM.dir/src/cSystem.o"
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/src/cTracking.cpp" "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/MultiCol-SLAM.dir/src/cTracking.o"
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/src/cViewer.cpp" "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/MultiCol-SLAM.dir/src/cViewer.o"
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/src/cam_model_omni.cpp" "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/MultiCol-SLAM.dir/src/cam_model_omni.o"
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/src/cam_system_omni.cpp" "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/MultiCol-SLAM.dir/src/cam_system_omni.o"
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/src/g2o_MultiCol_sim3_expmap.cpp" "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/MultiCol-SLAM.dir/src/g2o_MultiCol_sim3_expmap.o"
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/src/g2o_MultiCol_vertices_edges.cpp" "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/MultiCol-SLAM.dir/src/g2o_MultiCol_vertices_edges.o"
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/src/mdBRIEFextractorOct.cpp" "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/MultiCol-SLAM.dir/src/mdBRIEFextractorOct.o"
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/src/misc.cpp" "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/MultiCol-SLAM.dir/src/misc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../ThirdParty/OpenGV/include"
+  "../ThirdParty/g2o"
+  "../ThirdParty/Eigen"
+  "../"
+  "../include"
+  "../ThirdParty"
+  "/usr/local/lib/cmake/Pangolin/../../../include"
+  "/usr/local/include/opencv4"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ MultiCol_SLAM/build/CMakeFiles/multi_col_slam_lafida.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/Examples/Lafida/mult_col_slam_lafida.cpp" "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/multi_col_slam_lafida.dir/Examples/Lafida/mult_col_slam_lafida.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../ThirdParty/OpenGV/include"
+  "../ThirdParty/g2o"
+  "../ThirdParty/Eigen"
+  "../"
+  "../include"
+  "../ThirdParty"
+  "/usr/local/lib/cmake/Pangolin/../../../include"
+  "/usr/local/include/opencv4"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/grg/grg/SLAM/MultiCol/MultiCol-SLAM/build/CMakeFiles/MultiCol-SLAM.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
